4d85f70399827c93f35e17f7cc3932365591f946
tnorF <odewodzka@gmail.com>
2023-02-14 09:17:22 +0100

    Form validation

---
+++ script.js
@@ -0,0 +1,97 @@
+const username = document.querySelector("#username");
+const pass = document.querySelector("#password");
+const pass2 = document.querySelector("#password2");
+const email = document.querySelector("#email");
+const sendBtn = document.querySelector(".send");
+const clearBtn = document.querySelector(".clear");
+const popup = document.querySelector(".popup");
+
+const showError = (input, msg) =>
+	// argument msg przechowuje placeholder
+	{
+		const formBox = input.parentElement;
+		const errorMsg = formBox.querySelector(".error-text");
+
+		formBox.classList.add("error");
+		errorMsg.textContent = msg;
+	};
+
+const clearError = (input) => {
+	const formBox = input.parentElement;
+	formBox.classList.remove("error");
+};
+
+const checkForm = (input) => {
+	input.forEach((el) => {
+		if (el.value === "") {
+			showError(el, el.placeholder);
+		} else {
+			clearError(el);
+		}
+	});
+};
+
+const checkLength = (input, min) => {
+	if (input.value.length < min) {
+		showError(
+			input,
+			`${input.previousElementSibling.innerText.slice(
+				0,
+				-1
+			)} should contains min. ${min} characters.`
+		);
+	}
+};
+
+const checkPassword = (pass1, pass2) => {
+	if (pass.value !== pass2.value) {
+		showError(pass2, "Passwords do not match.");
+	}
+};
+
+const checkMail = (email) => {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
+
+	if (re.test(email.value)) {
+		clearError(email);
+	} else {
+		showError(email, "Email is not valid.");
+	}
+}
+const checkErrors = () => {
+	const allInputs = document.querySelectorAll(".form-box");
+	let errorCount = 0;
+
+	allInputs.forEach((el) => {
+		if (el.classList.contains("error")) {
+			errorCount++;
+		}
+	})
+
+    if(errorCount === 0) {
+        popup.classList.add('show-popup')
+    }
+}
+
+// argument input z funkcji checkForm przechowuje tablicę z inputami
+// argument el odnosi się do każdej zmiennej z tablicy
+sendBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+
+	checkForm([username, pass, pass2, email]);
+	checkLength(username, 3);
+	checkLength(pass, 8);
+	checkPassword(pass, pass2);
+	checkMail(email);
+    checkErrors();
+});
+// brak przeładowywania się www
+clearBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	//  czyszczenie wartości inputów
+	[username, pass, pass2, email].forEach((el) => {
+		el.value = "";
+		clearError(el);
+	});
+});
